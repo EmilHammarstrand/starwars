dbaf918f572962c017c386313739049584a55c93
Emil Hammarstrand <emilhammarstrand@hotmail.com>
2020-05-10 23:34:39 +0200

    A very late commit, did much today cant write it all

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,7 +7,14 @@ import axios from 'axios';
         super(props);
         this.state = {
             peopleList: [],
-            search: ""
+            search: "",
+            favoriteList: [],
+            showFavorites: false,
+            name: "",
+            age: "",
+            birthyear: "",
+            gender: "",
+            eyecolor: ""
         }
     }
 
@@ -20,13 +27,59 @@ import axios from 'axios';
         
         axios.get(baseUrl + 'people')
         .then(response => {
-            const peopleList = response.data.results || []
+            const peopleList = response.data.results
             this.setState({ peopleList: peopleList})
+            console.log("data: ", peopleList)
         })
     }
 
-    onBtnPress = event => {
+    onClick = () => {
+        this.addFavorites();
+        this.showFavFunciton();
+    }
+
+    handleChange = event => {
+        this.setState({
+            name: event.target.value,
+            age: event.target.value,
+            birthyear: event.target.value,
+            gender: event.target.value,
+            eyecolor: event.target.value});
+    }
+
+    formValidation = () => {
+        let asd = !this.state.name
+            && !this.state.age
+            && !this.state.birthyear
+            && !this.state.gender
+            && !this.state.eyecolor;
+        return asd;
+    }
+
+    showFavFunciton = () => {
+        this.setState({showFavorites: true})
+    }
+
+    onKeyboardClick = event => {
         this.setState({ search: event.target.value}); 
+    }
+
+    addFavorites = () => {
+        let favoriteList = this.state.favoriteList.concat(this.state.peopleList)
+        this.setState({ favoriteList })
+    }
+
+    addNewCharacter = () => {
+        this.setState(prevState => ({
+            favoriteList: [...prevState.favoriteList, {
+                name: this.state.name,
+                age: this.state.age,
+                birthyear: this.state.birthyear,
+                gender: this.state.gender,
+                eyecolor: this.state.eyecolor
+            }]
+        }))
+        this.setState({showFavorites: true})
     }
 
     filterPerson = person => {
@@ -43,21 +96,31 @@ import axios from 'axios';
             <main>
                 <div className="resultbox">
                     <ul>
-                        {this.state.peopleList.filter(this.filterPerson).map((person, id) => (
+                        {this.state.peopleList.filter(this.filterPerson).map( (person, id) => (
                             <li key={id}>
+                                <button className="fav" onClick={this.onClick}>Fav</button>
                                 {`(${person.gender}) ${person.name} is ${person.height} cm tall, got ${person.eye_color} eyes and is born ${person.birth_year}`}
                             </li>
                         ))}
+                        
+                        {this.state.showFavorites && <h3 className="favoriteHeader">Favorites</h3>}
+                        {this.state.favoriteList.map((person, id) => (
+                            <li key={id}>
+                                {`(${person.gender}) ${person.name}`}
+                                {`(${this.state.gender}) ${this.state.name} is ${this.state.height} cm tall, got ${this.state.eyecolor} eyes and is born ${this.state.birthyear}`}
+                            </li>
+                            ))}
                     </ul>
                 </div>
                 <div className="Form">
-                    <input className="search" placeholder="Search the universe!" type="text" value={this.state.search} onChange={this.onBtnPress}></input>
+                    <input className="search" placeholder="Search the universe!" type="text" value={this.state.search} onChange={this.onKeyboardClick}></input>
                     <h3>Make you own character!</h3>
-                    <input className="search special" placeholder="Name..." type="text" ></input>
-                    <input className="search" placeholder="Age..."></input>
-                    <input className="search" placeholder="Birthyear..."></input>
-                    <input className="search" placeholder="Gender..."></input>
-                    <button className="findBtn">Add character</button>
+                    <input className="search special" placeholder="Name..." onChange={this.handleChange}></input>
+                    <input className="search" placeholder="Gender..." onChange={this.handleChange}></input>
+                    <input className="search" placeholder="Birthyear..." onChange={this.handleChange}></input>
+                    <input className="search" placeholder="Height..." onChange={this.handleChange}></input>
+                    <input className="search" placeholder="Eyecolor..." onChange={this.handleChange}></input>
+                    <button className="findBtn" onClick={this.addNewCharacter} disabled={this.formValidation()}>Add character</button>
                 </div>
             </main>
         )
